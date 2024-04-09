7e75281636ea793a5b227e0882d40aa790920528
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-04-09 16:54:15 +0000

    First commit

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/Product.js");
+const app = express();
+app.use(express.json());
+
+const port = 4000;
+
+// Connect to MongoDB database using Mongoose
+const uri =
+  "mongodb+srv://khourssaoussama7:Ot4ar5QDXhfKX1n7@cluster0.s2tixsp.mongodb.net/chosm";
+
+mongoose.connect(uri).then(() => {
+  console.log(`Connected successfully to server`);
+});
+
+app.get("/test", (req, res) => {
+  res.json("Hello World!");
+});
+
+app.post("/add-product", async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json("New product added!");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/get-products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// api side port
+app.listen(port, () => {
+  console.log("Server is running on Port ", port);
+});
---
+++ api/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the product model
+module.exports = mongoose.model("Product", productSchema);
